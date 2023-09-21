ba98a9e3829b596a0b329592d3ae5d418aa7db12
MrMcStones <rasmus.ke.nilsson@gmail.com>
2023-09-21 17:24:52 +0200

    push changes up to and including task #4

--- src/com/rasmus/demo/Main.java
+++ src/com/rasmus/demo/Main.java
@@ -7,17 +7,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Player player = new Player();
+        boolean isPlaying = true;
 
         do {
             System.out.println("Insert your name please");
-            String result = sc.nextLine();
+            String result =sc.nextLine();
 
             player.name = result;
 
-            System.out.println("Your name is: " + player.name);
-            System.out.println();
+            System.out.println("Write a number you'd like to loop");
+            int loop = sc.nextInt();
 
-        } while (true);
+            sc.nextLine();
+
+            player.loop = loop;
+
+            System.out.println("Player #1: " + player.name);
+
+            for (int i = 1; i <= loop; i++) {
+                System.out.println("Loop# " +i);
+            }
+
+            System.out.println("Write 'stop' to stop application");
+            String stop = sc.nextLine();
+
+            if (stop.equalsIgnoreCase("stop")) {
+                isPlaying = false;
+            }
+
+        } while (isPlaying);
 
         /*while (!sc.hasNextInt()) {
             System.out.println("That's not a number!");
--- src/com/rasmus/demo/Player.java
+++ src/com/rasmus/demo/Player.java
@@ -2,4 +2,8 @@ package com.rasmus.demo;
 
 public class Player {
     public String name;
+
+    public int loop;
+
+    public boolean isPlaying;
 }
